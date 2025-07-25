5a025b6a42302411887b2353991d5c8d9c71c5ad
uukah <uukah@student.kit.edu>
2025-07-25 23:48:28 +0200

    Fixed overlapping in main task

--- experimental-task/choiceReactionTask/__init__.py
+++ experimental-task/choiceReactionTask/__init__.py
@@ -325,4 +325,4 @@ class SartMT(Page):
         player.timings_json = json.dumps(existing)
 
 
-page_sequence = [Sart, ResultsWaitPage, Results]
+page_sequence = [ReactionTimeMT, ResultsWaitPage, Results]
